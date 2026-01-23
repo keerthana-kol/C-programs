8573c963c43309c40192be9e5abf0c04792759e9
keerthana ☆ <keerthanakolanjiyappan@gmail.com>
2026-01-23 22:44:27 +0530

    Add example input/output to common difference program
    Added an example input and output for clarity.

--- basics/06_common_difference.c
+++ basics/06_common_difference.c
@@ -11,3 +11,10 @@ int main()
         printf("the common difference between %d and %d is %d\n",n1,n2,cd);
         return 0;
 }
+/*example:
+   input:
+ enter the first number : 15
+ enter the second number :3
+   output:
+the common difference between 15 and 3 is -12
+        */
